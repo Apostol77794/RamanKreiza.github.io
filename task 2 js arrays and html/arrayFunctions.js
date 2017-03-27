9a88f648a8375cd3cb13b0e327eb11715c41b2d1
Raman <creiziroma@gmail.com>
2017-03-27 23:33:26 +0300

    update arrayFun

--- task 2 js arrays and html/arrayFunctions.js
+++ task 2 js arrays and html/arrayFunctions.js
@@ -1,52 +1,54 @@
 'use strict';
 
-function isArray(val) {
-  return {}.toString.call(val) == "[object Array]";
+function isArray(array) {
+  return Object.prototype.toString.call(array) == "[object Array]";
 }
 
 console.log("\nisArray");
 console.log(isArray([1, 2, 3]));
 console.log(isArray(5));
 
-function isArray2(val) {
-  return val instanceof Array;
+function isArray2(array) {
+  return array instanceof Array;
 }
 
 console.log("\nisArray2");
 console.log(isArray2([1, 2, 3]));
 console.log(isArray2(5));
 
-function range(a, b, step = 1) {
-  var mas = []
+function range(a, b, step) {
+  step = step > 0 ? step : 1;
+  var newArray = []
   if (typeof b == 'undefined') {
     b = a;
     a = 0;
   }
   for (var i = a; i < b; i += step) {
-    mas.push(i);
+    newArray.push(i);
   }
-  return mas;
+  return newArray;
 }
 
 console.log("\nRange");
 console.log(range(5));
 console.log(range(1, 5));
 console.log(range(1, 10, 2));
+console.log(range(10, 20, -2));
 
 function compact(mas) {
   return mas.filter(function(x) {
-    return !!(x);
+    return x;
   });
 }
 
-function compactCycly(mas) {
-  var mas2 = [];
-  mas.forEach(function(item) {
-    if (Boolean(item).valueOf()) {
-      mas2.push(item);
+function compactCycly(array) {
+  var newArray = [];
+  array.forEach(function(item) {
+    if (item) {
+      newArray.push(item);
     }
   })
-  return mas2;
+  return newArray;
 }
 
 console.log("\nCompact");
@@ -59,15 +61,15 @@ console.log(compactCycly([{}, false, 1, 0, false]));
 console.log(compactCycly([false, 0, 1, {}]));
 
 
-function sum(mas) {
-  return mas.reduce(function(prev, next) {
+function sum(array) {
+  return array.reduce(function(prev, next) {
     return prev + next;
   })
 }
 
-function sumCycle(mas) {
+function sumCycle(array) {
   var sum = 0;
-  mas.forEach(function(item) {
+  array.forEach(function(item) {
     sum += item;
   });
   return sum;
@@ -79,8 +81,8 @@ console.log(sum([1, 2, 3, 0]));
 console.log("sumCycle");
 console.log(sumCycle([1, 2, 3, 0]));
 
-function unique(mas) {
-  return mas.filter(function(value, index, array) {
+function unique(array) {
+  return array.filter(function(value, index, array) {
     return array.indexOf(value) === index;
   });
 }
@@ -88,8 +90,8 @@ function unique(mas) {
 console.log("\nUnique");
 console.log(unique([1, 2, 3, 0, 1, 2]));
 
-function last(mas) {
-  return mas[mas.length - 1];
+function last(array) {
+  return array[array.length - 1];
 }
 
 console.log("\nLast");
@@ -97,7 +99,8 @@ console.log(last([1, 2, 3, 0, 1, 2]));
 console.log(last([1]));
 console.log(last([]));
 
-function excludeLast(mas, num = 1) {
+function excludeLast(mas, num) {
+  num = (typeof num !== "undefined") ? num : 1;
   var res = mas.slice(0, num * -1);
   return res;
 }
