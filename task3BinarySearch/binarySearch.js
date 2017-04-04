74ca58024c09b6bb143ab6a276cc10b066f1aa05
Raman <creiziroma@gmail.com>
2017-04-04 22:51:37 +0300

    delete wrap function

--- task3BinarySearch/binarySearch.js
+++ task3BinarySearch/binarySearch.js
@@ -28,12 +28,12 @@ console.log('iterSearch([2, 5, 2], 1) -> ' + iterSearch([2, 5, 2], 1));
 console.log('iterSearch([2, 5, 2], 5) -> ' + iterSearch([2, 5, 2], 5));
 console.log('iterSearch([2, 5, 2], 2) -> ' + iterSearch([2, 5, 2], 2));
 
-function recSearchWrap(array, number) {
-  return recSearch(array, number, 0, array.length);
-}
-
 function recSearch(array, number, left, right) {
   var mid;
+  if (arguments.length <= 2) {
+    left = 0;
+    right = array.length;
+  }
   if (left < right) {
     mid = Math.floor((left + right) / 2);
     if (number <= array[mid]) {
@@ -50,13 +50,13 @@ function recSearch(array, number, left, right) {
   }
 }
 
-console.log('\nrecSearchWrap([1,2,3,4,5,15,25,30], 4) -> ' + recSearchWrap([1, 2, 3, 4, 5, 15, 25, 30], 4));
-console.log('recSearchWrap([1,2,3,4,5,15,25,30], 1) -> ' + recSearchWrap([1, 2, 3, 4, 5, 15, 25, 30], 1));
-console.log('recSearchWrap([1,2,3,4,5,15,25,30], 30) -> ' + recSearchWrap([1, 2, 3, 4, 5, 15, 25, 30], 30));
-console.log('recSearchWrap([1], 30) -> ' + recSearchWrap([1], 30));
-console.log('recSearchWrap([1], 1) -> ' + recSearchWrap([1], 1));
-console.log('recSearchWrap([1,2], 1) -> ' + recSearchWrap([1, 2], 1));
-console.log('recSearchWrap([], 1) -> ' + recSearchWrap([], 1));
-console.log('recSearchWrap([2, 5, 2], 1) -> ' + recSearchWrap([2, 5, 2], 1));
-console.log('recSearchWrap([2, 5, 2], 5) -> ' + recSearchWrap([2, 5, 2], 5));
-console.log('recSearchWrap([2, 5, 2], 2) -> ' + recSearchWrap([2, 5, 2], 2));
+console.log('\nrecSearch([1,2,3,4,5,15,25,30], 4) -> ' + recSearch([1, 2, 3, 4, 5, 15, 25, 30], 4));
+console.log('recSearch([1,2,3,4,5,15,25,30], 1) -> ' + recSearch([1, 2, 3, 4, 5, 15, 25, 30], 1));
+console.log('recSearch([1,2,3,4,5,15,25,30], 30) -> ' + recSearch([1, 2, 3, 4, 5, 15, 25, 30], 30));
+console.log('recSearch([1], 30) -> ' + recSearch([1], 30));
+console.log('recSearch([1], 1) -> ' + recSearch([1], 1));
+console.log('recSearch([1,2], 1) -> ' + recSearch([1, 2], 1));
+console.log('recSearch([], 1) -> ' + recSearch([], 1));
+console.log('recSearch([2, 5, 2], 1) -> ' + recSearch([2, 5, 2], 1));
+console.log('recSearch([2, 5, 2], 5) -> ' + recSearch([2, 5, 2], 5));
+console.log('recSearch([2, 5, 2], 2) -> ' + recSearch([2, 5, 2], 2));
